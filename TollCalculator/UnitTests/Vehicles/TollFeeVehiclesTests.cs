9ba376f22da171e02afa50f4dd7d6a1be47b7c61
Gustav Nyberg <gustav.nyberg@electronomics.com>
2024-05-20 00:54:05 +0200

    Adding Vehicle tests.

---
+++ TollCalculator/UnitTests/Vehicles/TollFeeVehiclesTests.cs
@@ -0,0 +1,69 @@
+﻿namespace TollCalculator.UnitTests.Vehicles;
+
+using FluentAssertions;
+using Library;
+using TollCalculator.Library.Vehicles.NonTollFree;
+using TollCalculator.Library.Vehicles.TollFree;
+using Xunit;
+
+public class TollFeeVehiclesTests
+{
+    public class VehiclesBaseTests
+    {
+        [Fact]
+        public void IsTollFreeVehicle_ShouldReturnFalse_ForCar()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var result = car.IsTollFree();
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsTollFreeVehicle_ShouldReturnTrue_ForMotorbike()
+        {
+            // Arrange
+            var motorbike = new Motorbike();
+
+            // Act
+            var result = motorbike.IsTollFree();
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("Motorbike", true)]
+        [InlineData("Tractor", true)]
+        [InlineData("Emergency", true)]
+        [InlineData("Diplomat", true)]
+        [InlineData("Foreign", true)]
+        [InlineData("Military", true)]
+        [InlineData("Car", false)]
+        public void IsTollFreeVehicle_ShouldReturnExpectedResult(string vehicleType, bool expectedResult)
+        {
+            // Arrange
+            VehicleBase vehicle = vehicleType switch
+            {
+                "Motorbike" => new Motorbike(),
+                "Tractor" => new Tractor(),
+                "Emergency" => new Emergency(),
+                "Diplomat" => new Diplomat(),
+                "Foreign" => new Foreign(),
+                "Military" => new Military(),
+                "Car" => new Car(),
+                _ => throw new ArgumentOutOfRangeException(nameof(vehicleType), vehicleType, null)
+            };
+
+            // Act
+            var result = vehicle.IsTollFree();
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ TollCalculator/UnitTests/Vehicles/Types/VehicleTests.cs
@@ -0,0 +1,62 @@
+﻿namespace TollCalculator.UnitTests.Vehicles.Types;
+
+using FakeItEasy;
+using FluentAssertions;
+using TollCalculator.Library.Vehicles.NonTollFree;
+using TollCalculator.Library.Vehicles.TollFree;
+using Xunit;
+
+public class VehicleTests
+{
+    [Fact]
+    public void Car_GetVehicleType_ShouldReturnCar()
+    {
+        // Arrange
+        var car = new Car();
+
+        // Act
+        var result = car.GetVehicleType();
+        // Assert
+        result.Should().Be("Car");
+    }
+    [Fact]
+    public void Motorbike_GetVehicleType_ShouldReturnMotorbike()
+    {
+        // Arrange
+        var motorbike = new Motorbike();
+        
+        // Act
+        var result = motorbike.GetVehicleType();
+        
+        // Assert
+        result.Should().Be("Motorbike");
+    }
+
+    [Fact]
+    public void FakeCar_GetVehicleType_ShouldReturnCar()
+    {
+        // Arrange
+        var fakeCar = A.Fake<Car>();
+        A.CallTo(() => fakeCar.GetVehicleType()).Returns("Car");
+        
+        // Act
+        var result = fakeCar.GetVehicleType();
+        
+        // Assert
+        result.Should().Be("Car");
+    }
+    
+    [Fact]
+    public void FakeMotorbike_GetVehicleType_ShouldReturnMotorbike()
+    {
+        // Arrange
+        var fakeMotorbike = A.Fake<Car>();
+        A.CallTo(() => fakeMotorbike.GetVehicleType()).Returns("Motorbike");
+        
+        // Act
+        var result = fakeMotorbike.GetVehicleType();
+        
+        // Assert
+        result.Should().Be("Motorbike");
+    }
+}
